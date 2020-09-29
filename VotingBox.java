30a84df809ff4aeda99c943955006693e79558c8
Raquel Hernandez <raquel.hernandez2816@gmail.com>
2020-09-28 19:25:44 -0500

    first commit

---
+++ VotingBox.java
@@ -0,0 +1,45 @@
+import stanford.karel.*;
+
+public class VotingBox extends SuperKarel {
+
+	public void run() {
+		move();
+		move();
+		move();
+		move();
+		move();
+		turnLeft();
+		move();
+		pickBeeper();
+		turnLeft();
+		turnLeft();
+		move();
+		turnLeft();
+		move();
+		move();
+		move();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		pickBeeper();
+		pickBeeper();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		pickBeeper();
+		turnLeft();
+		turnLeft();
+		move();
+		turnLeft();
+		move();
+	
+	}
+	
+}
+	
+	
+	
+
